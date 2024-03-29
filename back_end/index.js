d5390724ab9054c9d9cd14594735aecac57bc00b
alijr2018 <alijr_2018@proton.me>
2024-03-29 23:58:51 +0000

    set up package.json and npm install

---
+++ back_end/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+const port = 8000;
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("Connection successefully"))
+.catch((err) => console.log(err));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.listen(port, () =>{
+    console.log(`Server is running on port: ${port}`);
+});
