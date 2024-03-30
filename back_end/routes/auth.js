af455395df083f71b2840ad1995add2f194fc998
alijr2018 <alijr_2018@proton.me>
2024-03-30 17:34:45 +0000

    handel GET request

--- back_end/routes/auth.js
+++ back_end/routes/auth.js
@@ -1,48 +1,46 @@
-const router = require("express").Router();
-const bcrypt = require("bcrypt");
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const User = require("../models/User");
+const User = require('../models/User');
 
+// Login
+router.post('/login', async (req, res) => {
+  const user = await user.findOne({ email: req.body.email });
+  if (user) {
+    const crypt = bcrypt.compare(user.password, req.body.password);
 
-//Login
-router.post("/login", async (req, res) => {
-    const user = await user.findOne({ email: req.body.email});
-    if (user) {
-        const crypt = bcrypt.compare(user.password, req.body.password);
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      'key',
+      { expiresIn: '24h' },
+    );
 
-        const token = jwt.sign(
-            { id: user._id, isAdmin: user.isAdmin },
-            'key',
-            {expiresIn: "24h"}
-        );
-
-        const {password, ...info } = user._doc;
-        if (!crypt) {
-            res.status(400).json("Your email or password are Wrong check well!")
-        } else {
-            res.status(200).json({ ...info, token });
-        }
+    const { password, ...info } = user._doc;
+    if (!crypt) {
+      res.status(400).json('Your email or password are Wrong check well!');
     } else {
-        res.status(401).json("Please create your account")
+      res.status(200).json({ ...info, token });
     }
+  } else {
+    res.status(401).json('Please create your account');
+  }
 });
 
+// Register
+router.post('/sing_up', async (req, res) => {
+  const encrypt = await bcrypt.genSalt(10);
+  const nUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: (await bcrypt.hash(req.body.password, encrypt)),
+  });
 
-//Register
-router.post("/sing_up", async (req, res) => {
-    const encrypt = await bcrypt.genSalt(10);
-    const nUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: (await bcrypt.hash(req.body.password, encrypt))
-    });
-
-    try{
-        const user = await nUser.save();
-        res.status(201).json(user);
-    } catch(err) {
-        res.status(500).json(err);
-    }
+  try {
+    const user = await nUser.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- back_end/routes/users.js
+++ back_end/routes/users.js
@@ -1,3 +1,20 @@
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
+const User = require("../models/User");
 
+//GET
+router.get("/", verify, async (req, res) => {
+    const que = req.query.new;
+    if (req.user.isAdmin) {
+        try {
+            const users = que ? await User.find().sort({_id:-1}).limit(5) : await User.find();
+            res.status(200).json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("Permession denided")
+    }
+});
+
+module.exports = router;
