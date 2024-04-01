cb00a5f5e5a0dfadfb1db1c6158331af19184adc
alijr2018 <alijr_2018@proton.me>
2024-04-01 17:20:57 +0000

    connection back_end and front_end

--- back_end/index.js
+++ back_end/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const auth = require("./routes/auth");
+const user = require("./routes/users");
+const agents = require("./routes/agents");
 
-const port = 1200;//choose the port you want
+const port = 3000;//choose the port you want
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
@@ -12,6 +15,10 @@ mongoose.connect(process.env.MONGO_URL)
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use("/api/auth", auth);
+app.use("/api/users", user);
+app.use("/api/agents", agents);
 
 app.listen(port, () =>{
     console.log(`Server is running on port: ${port}`);
