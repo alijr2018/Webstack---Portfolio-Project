586e31a41d344b63f9506a57eae8002c95d460c1
alijr2018 <alijr_2018@proton.me>
2024-04-05 06:46:49 +0000

    later edit

---
+++ front_end/Social/src/place/About/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+import AboutH from "./About.html";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import SearchBar from "../utils/SearchBar";
+
+function About() {
+    return (
+    <div className="About">
+      <Header />
+      {/* Render the HTML content */}
+      <div dangerouslySetInnerHTML={{ __html: AboutH }} />
+      <SearchBar header="Let's Find A Property That Meets Your Need" />
+      <Footer />
+    </div>
+  );
+}
+export default About;
